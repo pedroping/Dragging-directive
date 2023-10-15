92a5684f7d46b73f8023c0f8d1321744dab5b873
pedroping <pedrpchavesdias@gmail.com>
2023-10-15 11:29:56 -0300

    Refactor: subscriptions as callbacks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -82,10 +82,33 @@ import { Component } from "@angular/core";
   ],
 })
 export class AppComponent {
+  private _worker: Worker;
   title = "angular-free-dragging";
 
   baseSizes = {
     width: 500,
     height: 200,
   };
+
+  ngOnInit(): void {
+    this.setServiceWorker();
+  }
+
+  setServiceWorker() {
+    this._worker = new Worker(
+      new URL("./drag-calculator.worker", import.meta.url)
+    );
+    this._worker.postMessage("Hello World!!");
+    this._worker.onmessage = ({ data }) => {
+      if (this.istanceofString(data)) console.log(data);
+    };
+  }
+
+  istanceofString(data: unknown): data is string {
+    return !!data["substring"];
+  }
+
+  ngOnDestroy(): void {
+    this._worker.terminate();
+  }
 }
--- src/app/shared/free-dragging.directive.ts
+++ src/app/shared/free-dragging.directive.ts
@@ -5,9 +5,8 @@ import {
   ElementRef,
   Input,
   OnDestroy,
-  OnInit,
 } from "@angular/core";
-import { Subject, Subscription, fromEvent, timer } from "rxjs";
+import { Observable, Subject, Subscription, fromEvent, timer } from "rxjs";
 import { distinctUntilChanged, take, takeUntil } from "rxjs/operators";
 import { FreeDraggingHandleDirective } from "./free-dragging-handle.directive";
 
@@ -26,9 +25,8 @@ export const GAP = 10;
   selector: "[appFreeDragging]",
   exportAs: "appFreeDragging",
 })
-export class FreeDraggingDirective implements OnInit, AfterViewInit, OnDestroy {
+export class FreeDraggingDirective implements AfterViewInit, OnDestroy {
   private readonly DEFAULT_DRAGGING_BOUNDARY_QUERY = "html";
-  private _worker!: Worker;
   private element: HTMLElement;
   private subscriptions: Subscription[] = [];
 
@@ -44,11 +42,15 @@ export class FreeDraggingDirective implements OnInit, AfterViewInit, OnDestroy {
   stopTaking$ = new Subject<void>();
   isOnFullScreen = false;
 
-  constructor(private elementRef: ElementRef) { }
+  initialX: number;
+  initialY: number;
+  currentX = 0;
+  currentY = 0;
+  dragSub: Subscription;
 
-  ngOnInit(): void {
-    this.setServiceWorker();
-  }
+  observeConfig = { attributes: true, childList: true, subtree: true };
+
+  constructor(private elementRef: ElementRef) {}
 
   ngAfterViewInit(): void {
     this.draggingBoundaryElement = document.querySelector(this.boundaryQuery);
@@ -72,137 +74,27 @@ export class FreeDraggingDirective implements OnInit, AfterViewInit, OnDestroy {
     const drag$ = fromEvent<MouseEvent>(document, "mousemove").pipe(
       takeUntil(dragEnd$)
     );
-
     const resize$ = resizeSubject$
       .asObservable()
-      .pipe(distinctUntilChanged((prev, curr) => prev === curr));
-
-    let initialX: number,
-      initialY: number,
-      currentX = 0,
-      currentY = 0;
-
-    let dragSub: Subscription;
-
-    const dragStartSub = dragStart$.subscribe((event: MouseEvent) => {
-      const minBoundX = this.draggingBoundaryElement.offsetLeft;
-      const minBoundY = this.draggingBoundaryElement.offsetTop;
-
-      const maxBoundX =
-        minBoundX +
-        this.draggingBoundaryElement.offsetWidth -
-        this.element.offsetWidth;
-      const maxBoundY =
-        minBoundY +
-        this.draggingBoundaryElement.offsetHeight -
-        this.heightDrecrease -
-        this.element.offsetHeight;
-
-      this.stopTaking$.next();
-      initialX = event.clientX - currentX;
-      initialY = event.clientY - currentY;
-      this.element.classList.add("free-dragging");
-
-      dragSub = drag$.subscribe((event: MouseEvent) => {
-        this.stopTaking$.next();
-        event.preventDefault();
-
-        const x = event.clientX - initialX;
-        const y = event.clientY - initialY;
-
-        currentX = Math.max(minBoundX, Math.min(x, maxBoundX));
-        currentY = Math.max(minBoundY, Math.min(y, maxBoundY));
-
-        this.element.style.transform = `translate3d(${currentX}px, ${currentY}px, 0)`;;
-      });
-    });
-
-    const dragEndSub = dragEnd$.subscribe(() => {
-      initialX = currentX;
-      initialY = currentY;
-      this.stopTaking$.next();
-      this.element.classList.remove("free-dragging");
-      if (dragSub) {
-        dragSub.unsubscribe();
-      }
-    });
-
-    const resizeSub = resize$
       .pipe(
         distinctUntilChanged(
           (prev: ElementSizes, curr: ElementSizes) =>
             prev.height === curr.height && prev.width === curr.width
         )
-      )
-      .subscribe(() => {
-        const { x, y } = this.getTransformValues(this.element.style.transform);
-        currentX = x;
-        currentY = y;
+      );
 
-        if (
-          this.element.offsetWidth != window.innerWidth ||
-          this.element.offsetHeight != window.innerHeight - this.heightDrecrease
-        )
-          this.isOnFullScreen = false;
-
-        if (
-          y + this.element.offsetHeight >
-          window.innerHeight - this.heightDrecrease - GAP
-        ) {
-          const newY =
-            window.innerHeight -
-            this.heightDrecrease -
-            GAP -
-            this.element.offsetHeight +
-            GAP;
-          currentY = newY;
-        }
-
-        if (x + this.element.offsetWidth > window.innerWidth - GAP * 2) {
-          const newX = window.innerWidth - this.element.offsetWidth;
-          currentX = newX;
-        }
-
-        this.element.style.transform = `translate3d(${currentX}px, ${currentY}px, 0)`;
-      });
-
-    const windowResizeSub = windowResize$.subscribe(() => {
-      if (this.isOnFullScreen) {
-        this.setFullSize();
-        return;
-      }
-
-      if (this.element.offsetWidth > window.innerWidth) {
-        this.element.style.width = window.innerWidth + "px";
-      }
-
-      if (
-        this.element.offsetHeight >
-        window.innerHeight - this.heightDrecrease
-      ) {
-        this.element.style.height =
-          window.innerHeight - this.heightDrecrease + "px";
-      }
-    });
-
-    const config = { attributes: true, childList: true, subtree: true };
-
-    new MutationObserver(() => {
-      const width = this.elementRef.nativeElement.style.width;
-      const height = this.elementRef.nativeElement.style.height;
-      resizeSubject$.next({
-        width: this.isSmallestThan(width, this.baseSizes.width)
-          ? "200px"
-          : width,
-        height: this.isSmallestThan(height, this.baseSizes.height)
-          ? "200px"
-          : height,
-      });
-    }).observe(this.elementRef.nativeElement, config);
+    const dragStartSub = dragStart$.subscribe(this.dragStartCallBack(drag$));
+    const dragEndSub = dragEnd$.subscribe(this.dragEndCallBack());
+    const windowResizeSub = windowResize$.subscribe(this.winResizeCallBack());
+    const resizeSub = resize$.subscribe(this.resizeCallBack());
+    new MutationObserver(this.mutationObserveCallBack(resizeSubject$)).observe(
+      this.elementRef.nativeElement,
+      this.observeConfig
+    );
 
     this.subscriptions.push.apply(this.subscriptions, [
       dragStartSub,
-      dragSub,
+      this.dragSub,
       dragEndSub,
       resizeSub,
       windowResizeSub,
@@ -216,7 +108,6 @@ export class FreeDraggingDirective implements OnInit, AfterViewInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.subscriptions.forEach((s) => s?.unsubscribe());
-    this._worker.terminate();
   }
 
   getTransformValues(transform: string) {
@@ -250,17 +141,125 @@ export class FreeDraggingDirective implements OnInit, AfterViewInit, OnDestroy {
       });
   }
 
-  setServiceWorker() {
-    this._worker = new Worker(
-      new URL("../drag-calculator.worker", import.meta.url)
-    );
-    this._worker.postMessage("Hello World!!");
-    this._worker.onmessage = ({ data }) => {
-      if (this.istanceofString(data)) console.log(data);
+  dragStartCallBack(drag$: Observable<MouseEvent>) {
+    return (event: MouseEvent) => {
+      const minBoundX = this.draggingBoundaryElement.offsetLeft;
+      const minBoundY = this.draggingBoundaryElement.offsetTop;
+
+      const maxBoundX =
+        minBoundX +
+        this.draggingBoundaryElement.offsetWidth -
+        this.element.offsetWidth;
+      const maxBoundY =
+        minBoundY +
+        this.draggingBoundaryElement.offsetHeight -
+        this.heightDrecrease -
+        this.element.offsetHeight;
+
+      this.stopTaking$.next();
+      this.initialX = event.clientX - this.currentX;
+      this.initialY = event.clientY - this.currentY;
+      this.element.classList.add("free-dragging");
+
+      this.dragSub = drag$.subscribe((event: MouseEvent) => {
+        this.stopTaking$.next();
+        event.preventDefault();
+
+        const x = event.clientX - this.initialX;
+        const y = event.clientY - this.initialY;
+
+        this.currentX = Math.max(minBoundX, Math.min(x, maxBoundX));
+        this.currentY = Math.max(minBoundY, Math.min(y, maxBoundY));
+
+        this.element.style.transform = `translate3d(${this.currentX}px, ${this.currentY}px, 0)`;
+      });
     };
   }
 
-  istanceofString(data: unknown): data is string {
-    return !!data["substring"];
+  dragEndCallBack() {
+    return () => {
+      this.initialX = this.currentX;
+      this.initialY = this.currentY;
+      this.stopTaking$.next();
+      this.element.classList.remove("free-dragging");
+      if (this.dragSub) {
+        this.dragSub.unsubscribe();
+      }
+    };
+  }
+
+  resizeCallBack() {
+    return () => {
+      const { x, y } = this.getTransformValues(this.element.style.transform);
+      this.currentX = x;
+      this.currentY = y;
+
+      if (
+        this.element.offsetWidth != window.innerWidth ||
+        this.element.offsetHeight != window.innerHeight - this.heightDrecrease
+      )
+        this.isOnFullScreen = false;
+
+      if (
+        y + this.element.offsetHeight >
+        window.innerHeight - this.heightDrecrease - GAP
+      ) {
+        const newY =
+          window.innerHeight -
+          this.heightDrecrease -
+          GAP -
+          this.element.offsetHeight +
+          GAP;
+        this.currentY = newY;
+      }
+
+      if (x + this.element.offsetWidth > window.innerWidth - GAP * 2) {
+        const newX = window.innerWidth - this.element.offsetWidth;
+        this.currentX = newX;
+      }
+
+      this.element.style.transform = `translate3d(${this.currentX}px, ${this.currentY}px, 0)`;
+    };
+  }
+
+  winResizeCallBack() {
+    return () => {
+      if (this.isOnFullScreen) {
+        this.setFullSize();
+        return;
+      }
+
+      if (this.element.offsetWidth > window.innerWidth) {
+        this.element.style.width = window.innerWidth + "px";
+      }
+
+      if (
+        this.element.offsetHeight >
+        window.innerHeight - this.heightDrecrease
+      ) {
+        this.element.style.height =
+          window.innerHeight - this.heightDrecrease + "px";
+      }
+    };
+  }
+
+  mutationObserveCallBack(
+    resizeSubject$: Subject<{
+      height: string;
+      width: string;
+    }>
+  ) {
+    return () => {
+      const width = this.elementRef.nativeElement.style.width;
+      const height = this.elementRef.nativeElement.style.height;
+      resizeSubject$.next({
+        width: this.isSmallestThan(width, this.baseSizes.width)
+          ? `${this.baseSizes.width}px`
+          : width,
+        height: this.isSmallestThan(height, this.baseSizes.height)
+          ? `${this.baseSizes.height}px`
+          : height,
+      });
+    };
   }
 }
