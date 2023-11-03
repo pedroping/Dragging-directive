1f7fc896fca96808793c22cff4e49a33ea58a005
pedro.henrique <pedro.henrique@brunsker.com.br>
2023-11-03 16:55:56 -0300

    Feat: page footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './main-component/app.component';
 import { FreeDraggingHandleDirective } from './shared/directives/free-dragging-handle.directive';
 import { SharedModule } from './shared/shared.module';
 import { FreeDraggingDirective } from './shared/directives/free-dragging.directive';
 import { FreeDraggingSetFullScreenDirective } from './shared/directives/free-dragging-set-full-screen.directive';
+import { AppComponent } from './main-component/app.component';
 
 @NgModule({
   declarations: [
