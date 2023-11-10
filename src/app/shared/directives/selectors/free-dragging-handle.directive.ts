91e8dc6d5d143c098689d7b777751c7811ecd624
pedro.henrique <pedro.henrique@brunsker.com.br>
2023-11-10 12:41:44 -0300

    Feat: mouse grabbing on screen move

--- src/app/shared/directives/selectors/free-dragging-handle.directive.ts
+++ src/app/shared/directives/selectors/free-dragging-handle.directive.ts
@@ -1,9 +1,20 @@
-import { Directive } from "@angular/core";
+import { Directive, HostListener } from "@angular/core";
 
 @Directive({
   selector: "[appFreeDraggingHandle]",
   standalone: true
 })
 export class FreeDraggingHandleDirective {
-  constructor() { }
+
+  private _body = document.querySelector('body')
+
+  @HostListener('mousedown') private onMouseDown() {
+    this._body.style.cursor = 'grabbing';
+  }
+
+  @HostListener('mouseup')
+  @HostListener('mouseleave')
+  private onMouseUp() {
+    this._body.style.cursor = 'default';
+  }
 }
